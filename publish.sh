7a195a95a711bb07c6a9203cd99e869133d71ace
Alexandre de Verteuil <alexandre@deverteuil.net>
2019-12-08 22:23:47 -0500

    Fix publish.sh local path name

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 hugo
-rsync --verbose --recursive --chmod=ug=rwX,o=rX --delete output/ 192.168.13.14:public
+rsync --verbose --recursive --chmod=ug=rwX,o=rX --delete public/ 192.168.13.14:public
